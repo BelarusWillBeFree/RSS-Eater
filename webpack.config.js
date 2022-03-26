7b94e5f4e4cc7dc049d39983b6b1877d35f17ef3
Anatoli Muzika <javamuzman@gmail.com>
2022-03-26 20:35:16 +0300

    add main files

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line import/no-import-module-exports
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  devServer: {
+    hot: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
