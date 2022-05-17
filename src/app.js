a9f8479c380024a6cd0c9e6ff7b3313b585a98bd
Anatoli Muzika <javamuzman@gmail.com>
2022-05-17 06:58:22 +0300

    refactoring functions

--- src/app.js
+++ src/app.js
@@ -35,49 +35,54 @@ const getAllOriginsURL = (urlText) => {
   return urlFeed.toString();
 };
 
-const saveFeed = (parsingFeed, watchedState, url) => {
-  const foundFeed = watchedState.feeds.filter((feed) => (feed.url === url));
-  if (foundFeed.length) return [foundFeed];
+const createNewFeed = (parsingFeed, watchedState, url) => {
   const newFeed = {
     url,
     id: watchedState.id.feed,
     title: parsingFeed.title,
     description: parsingFeed.description,
   };
-  watchedState.feeds.push(newFeed);
-  watchedState.urlFeeds.push(url);
-  watchedState.id.feed += 1;
   return newFeed;
 };
 
-const addNewPosts = (parsingPosts, watchedState, feed) => {
-  const postsCurrFeed = watchedState.posts.filter((elem) => (elem.idFeed === feed.id));
-  const postsNeedAdd = _.differenceBy(parsingPosts, postsCurrFeed, 'link');
+const addOnlyNewPosts = (parsingPosts, watchedState, feed) => {
+  const savedPosts = watchedState.posts.filter((elem) => (elem.idFeed === feed.id));
+  const postsNeedAdd = _.differenceBy(parsingPosts, savedPosts, 'link');
 
   postsNeedAdd.forEach((item) => {
     item.idFeed = feed.id;
-    const idPost = `${feed.id}-${watchedState.id.post}`;
+    item.idPost = `${feed.id}-${watchedState.id.post}`;
     watchedState.id.post += 1;
-    item.idPost = idPost;
     watchedState.posts.push(item);
   });
 };
 
-const processingResponse = (response, watchedState, url) => {
+const saveFeedAndPosts = (response, watchedState, url) => {
   const { data } = response;
 
   const { parsingFeed, parsingPosts } = parsingRSS(data, watchedState);
   if (parsingFeed === undefined) return;
 
-  const feed = saveFeed(parsingFeed, watchedState, url);
-  addNewPosts(parsingPosts, watchedState, feed);
+  const newFeed = createNewFeed(parsingFeed, watchedState, url);
+  watchedState.feeds.push(newFeed);
+  watchedState.urlFeeds.push(url);
+  watchedState.id.feed += 1;
+
+  addOnlyNewPosts(parsingPosts, watchedState, newFeed);
 };
 
+const updatePosts = (response, watchedState, feed) => {
+  const { data } = response;
+
+  const { parsingFeed, parsingPosts } = parsingRSS(data, watchedState);
+  if (parsingFeed === undefined) return;
+  addOnlyNewPosts(parsingPosts, watchedState, feed);
+};
 const updatePostsByInterval = (watchedState, updateInterval) => {
   const { feeds } = watchedState;
   const arrPromises = feeds.map((feed) => axios(getAllOriginsURL(feed.url))
     .then((promise) => ({
-      result: 'succes', value: promise, url: feed.url,
+      result: 'succes', value: promise, feed,
     }))
     .catch((error) => ({
       result: 'error', error,
@@ -86,7 +91,7 @@ const updatePostsByInterval = (watchedState, updateInterval) => {
   resultPromise.then((arrResults) => {
     arrResults.forEach((onePromies) => {
       if (onePromies.result === 'succes') {
-        processingResponse(onePromies.value, watchedState, onePromies.url);
+        updatePosts(onePromies.value, watchedState, onePromies.feed);
       } else {
         watchedState.status = 'networkError';
         watchedState.view.message = 'error.networkError';
@@ -107,32 +112,36 @@ const initViewElements = (watchedState) => {
   watchedState.view.urlInput = document.getElementById('url-input');
 };
 
-const appendFeed = (watchedState, urlPath) => {
-  const schemaUrl = yup.string().required().url().trim()
-    .notOneOf(watchedState.urlFeeds);
+const requestByURL = (url, watchedState) => {
+  const allOriginsURL = getAllOriginsURL(url);
+  axios(allOriginsURL).then((response) => {
+    watchedState.status = 'urlAccess';
+    watchedState.view.message = 'message.urlAccess';
+    saveFeedAndPosts(response, watchedState, url);
+  })
+    .catch(() => {
+      watchedState.status = 'networkError';
+      watchedState.view.message = 'error.networkError';
+    });
+};
+
+const validateError = (e, watchedState) => {
+  if (e.type === 'notOneOf') {
+    watchedState.status = 'urlAlreadyExist';
+    watchedState.view.message = 'error.urlAlreadyExist';
+  } else {
+    watchedState.status = 'validationError';
+    watchedState.view.message = 'error.validationError';
+  }
+};
+
+const validateUrl = (url, watchedState) => {
+  const schemaUrl = yup.string().required().url().trim().notOneOf(watchedState.urlFeeds);
   watchedState.status = 'validation';
   watchedState.view.message = 'message.validation';
-  schemaUrl.validate(urlPath)
-    .then(() => {
-      axios(getAllOriginsURL(urlPath)).then((response) => {
-        watchedState.status = 'urlAccess';
-        watchedState.view.message = 'message.urlAccess';
-        processingResponse(response, watchedState, urlPath);
-      })
-        .catch(() => {
-          watchedState.status = 'networkError';
-          watchedState.view.message = 'error.networkError';
-        });
-    })
-    .catch((e) => {
-      if (e.type === 'notOneOf') {
-        watchedState.status = 'urlAlreadyExist';
-        watchedState.view.message = 'error.urlAlreadyExist';
-      } else {
-        watchedState.status = 'validationError';
-        watchedState.view.message = 'error.validationError';
-      }
-    });
+  schemaUrl.validate(url)
+    .then(() => requestByURL(url, watchedState))
+    .catch((e) => validateError(e, watchedState));
 };
 
 const main = () => {
@@ -159,7 +168,8 @@ const main = () => {
   form.addEventListener('submit', (objEvent) => {
     objEvent.preventDefault();
     const urlInput = document.getElementById('url-input');
-    appendFeed(watchedState, urlInput.value);
+    const url = urlInput.value;
+    validateUrl(url, watchedState);
   });
 };
 
